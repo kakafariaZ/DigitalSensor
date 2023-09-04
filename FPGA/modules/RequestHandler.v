66e425057be33d35d177df88f8d9a2a5f1f03df4
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-09-03 21:59:04 -0300

    feat(FPGA): Added place holders for the new modules.

---
+++ FPGA/modules/RequestHandler.v
@@ -0,0 +1,7 @@
+/**
+* This module implements a decoder for handling the requests given to the FPGA. The handling is
+* based on the established communication protocol found in - (LINK).
+*/
+module RequestHandler ();
+
+endmodule
---
+++ FPGA/modules/SensorDecoder.v
@@ -0,0 +1,9 @@
+/**
+* This module implements a high level facade for handling responses off up to 32 different sensors.
+*
+* NOTE: Currently only working with the DHT11 sensor, whose module is found in this project by the
+* name of `DHT11.v`.
+*/
+module SensorDecoder ();
+
+endmodule
